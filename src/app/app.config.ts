4fd3aa26aa684f9c64a44b2f8b78d3020e24645e
Ars-5 <manticora1999@gmail.com>
2024-01-09 19:43:03 -0500

    probando v17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"crudrfbv2-app","appId":"1:739370718848:web:c891977cfdf62793267865","storageBucket":"crudrfbv2-app.appspot.com","locationId":"us-central","apiKey":"AIzaSyDS6ng6f69n1V29CNvX7RXPOmUPdx0SXaY","authDomain":"crudrfbv2-app.firebaseapp.com","messagingSenderId":"739370718848"}))), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
